64217baea2ed784c4fc517c3c3207a6d0901f319
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-04-24 11:52:36 +0200

    add header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-console.log("ciao");
+// ? Milestone 1
+// Sfruttando gli screen e gli asset in allegato riproduciamo la grafica proposta in maniera statica:
+// utilizzando soltanto HTML e CSS e riproducendo una singola fotografia
+//  (usiamo una qualunque immagine a piacimento)
