347f412afd948fed69033357165dd8ba1d4156f9
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-04-28 17:05:25 +0200

    add end remove overlay

--- js/script.js
+++ js/script.js
@@ -15,10 +15,11 @@
 // Spostandosi col mouse sopra le foto, queste si zoommano, ruotano di 10 gradi e la loro ombra aumenta,
 //  il tutto in manierà fluida. Inoltre il mouse diventa un puntatore, per far capire all’utente che può cliccare.
 
-const loaderEl = document.querySelector(".overlay");
-
 const allCardEl = document.querySelector(".all-card");
 const cardsContainer = document.getElementById("cards-container");
+const overlayNoVisible = document.querySelector("overlay-no-visible");
+
+const closeButton = document.getElementById("close-button");
 
 const apiUri = `https://lanciweb.github.io/demo/api/pictures/`;
 
@@ -27,10 +28,12 @@ const apiUri = `https://lanciweb.github.io/demo/api/pictures/`;
 function generateCard(post) {
   const cardHtml = `
   <div class="col-xs-12 col-md-6 col-lg-4">
-            <div class="all-card">
+  
+            <div class="all-card" id="post-card-${post.id}" >
               <div class="card-image">
+                
+                <img src="${post.url}"  alt="${post.title}" />
                 <img class="pin-img" src="./img/pin.svg" alt="pin" />
-                <img src="${post.url}" alt="${post.title}" />
               </div>
               <div class="card-text py-3">
                 <time class="data text-secondary">${post.date}</time>
@@ -45,31 +48,44 @@ function generateCard(post) {
   return cardHtml;
 }
 
-axios
-  .get(apiUri)
-  .then((response) => {
-    const postsCard = response.data;
-    console.log(postsCard);
+// # CHIAMATA AJAX
 
-    let cardsHtml = "";
-    postsCard.forEach((postCard) => {
-      cardsHtml += generateCard(postCard);
-    });
-    // innerHtml è una DOM API, faccio in modo che la pagina venga aggiornata una volta sola
-    //anzichè 6 volte, quindi lo inserisco fuori dal ciclo
-    cardsContainer.innerHTML += cardsHtml;
-  })
+axios.get(apiUri).then((response) => {
+  const postsCard = response.data;
+  console.log(postsCard);
 
-  // const cardTitle = response.data.title;
-  // const cardDate = response.data[0].date;
-  // const cardUrl = response.data[0].url;
-  // const newImage = generateCard(cardTitle, cardDate, cardUrl);
-  // allCardEl.innerHTML = newImage;
-
-  .cacth((error) => {
-    console.log(error);
-    alert("Errore!!");
-  })
-  .finally(() => {
-    loaderEl.classList.remove("d-none");
+  let cardsHtml = "";
+  postsCard.forEach((postCard) => {
+    cardsHtml += generateCard(postCard);
   });
+  // innerHtml è una DOM API, faccio in modo che la pagina venga aggiornata una volta sola
+  //anzichè 6 volte, quindi lo inserisco fuori dal ciclo
+  cardsContainer.innerHTML += cardsHtml;
+
+  // creo una lista di nodi di postsCard
+  const postsCardNodes = document.querySelectorAll(".all-card");
+  console.log(postsCardNodes);
+
+  // scorro la lista di nodi e ad ogni nodo aggiungo l'addEventListener
+  postsCardNodes.forEach((postCardNode) => {
+    postCardNode.addEventListener("click", () => {
+      // salvo l'url dell'immagine selezionata al click in imgSrc
+      const imgSrc = postCardNode.querySelector("img").src;
+
+      console.log(imgSrc);
+      // rimuovo la classe overlay-no-visible
+      document.querySelector(`.overlay`).classList.remove(`overlay-no-visible`);
+      document.querySelector(`.overlay img`).src = imgSrc;
+    });
+    // al click del bottone l'overlay si chiude
+    closeButton.addEventListener("click", () => {
+      document.querySelector(`.overlay`).classList.add(`overlay-no-visible`);
+    });
+  });
+});
+
+// .cacth((error) => {
+//   console.log(error);
+//
+// })
+// .finally(() => {});
